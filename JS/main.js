3406a0071e4073579b3981b63388bd32a5770fd7
samandar12 <samandarkomiljonov1212@gmail.com>
2023-05-17 22:04:20 +0500

    Bu js7-dars kodlari yuklandi

---
+++ JS/main.js
@@ -0,0 +1,62 @@
+/* Matin uzunligini topish */
+
+/*let matn = "Assalomu Aleykum Shogirtlar"
+console.log(matn.length);*/
+
+/* indexOF (matn ichidan sozning boshlanish indexsini topish) */
+
+/*let matn = "Sen gapir men esa sen bilan yozib boraman";
+let birinchiSenIndex = matn.indexOf('sen')
+console.log(matn.indexOf('sen',birinchiSenIndex+1));*/
+
+/* replace (so'zlarni almashtirib qo'yish) */
+
+/*let matn = "Salom hammaga! salom berdim hammaga! salom";*/
+
+/*console.log(matn.replace(/salom|hammaga|berdim/gi,'alik'));
+let yangiMatn = matn.replace(/salom|hammaga/gi, function (soz){
+   if (soz =='Salom' || soz == 'salom'){
+       return 'alik';
+   }
+});*/
+
+/*let list = []
+
+list.push('Firemen');
+list.push('Freedom');
+list.push('Police');
+list.push('Oloviddin ayronchi');
+list.push('Samandar');
+list.push('Ozodbek');
+
+console.log(list);
+console.log(list.length);
+
+console.log(list.pop());
+console.log(list);
+console.log(list.pop());
+console.log(list);
+console.log(list.pop());
+console.log(list);*/
+
+/*let list = [];
+list.unshift('Olov');
+list.unshift('Komol');
+list.unshift('Bekzod');
+list.unshift(14);
+console.log(list);
+console.log(list.shift());
+console.log(list);
+console.log(list.shift());
+console.log(list);
+console.log(list.shift());*/
+
+/*let list = ['Komoliddin','Samandar','Oloviddin','Ozodbek','Bekzod','Shaxzod'];
+console.log(list.reverse() sort());*/
+
+/*let sonlar= [6,89,75,67,65,3,2];
+let tartiblangansonlar = sonlar.sort(function (a,b){
+    return a-b;
+});
+console.log(tartiblangansonlar.reverse(), sort() );*/
+
